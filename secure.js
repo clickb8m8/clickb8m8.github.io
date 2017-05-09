922f8288e37b0395a2c8d586ee5d9f83983a5adf
clickb8m8 <clickb8mail@gmail.com>
2017-05-08 22:14:16 -0500

    added article

---
+++ secure.js
@@ -0,0 +1,6 @@
+function ff(){
+    return false;
+}
+
+document.body.oncopy = ff;
+document.body.oncut = ff;
---
+++ share.js
@@ -0,0 +1,19 @@
+function showContent(){
+    var cont = document.getElementsByClassName("content");
+    for(var n = 0; n < cont.length; n++){
+	cont[n].style.backgroundColor = "grey";
+    }
+}
+
+var elms = document.getElementsByClassName("share");
+for(var i = 0; i < elms.length; i++){
+    elms[i].innerHTML = "Share to view content: <br><a href='https://www.facebook.com/sharer/sharer.php?u="
+	+ document.URL
+	+ "' target='_blank'>"
+	+ "<img class='shrimg' src='share-on-facebook.png'></img></a>";
+}
+
+var elms = document.getElementsByClassName("shrimg");
+for(var i = 0; i < elms.length; i++){
+    elms[i].onclick = showContent;
+}
