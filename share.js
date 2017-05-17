ebb7ad37da0b9cc4e2c3dcab5aa2b7619b1578ac
clickb8m8 <clickb8mail@gmail.com>
2017-05-16 22:48:15 -0500

    more articles

--- share.js
+++ share.js
@@ -7,10 +7,11 @@ function showContent(){
 
 var elms = document.getElementsByClassName("share");
 for(var i = 0; i < elms.length; i++){
-    elms[i].innerHTML = "<a href='https://www.facebook.com/sharer/sharer.php?u="
+    elms[i].innerHTML = "<a href='http://clickb8m8.github.io/index.html'>Main Page</a><br>"
+	+ "<a href='https://www.facebook.com/sharer/sharer.php?u="
 	+ document.URL
 	+ "' target='_blank'>"
-	+ "<img class='shrimg' src='share-on-facebook.png'></img></a>";
+	+ "<img class='shrimg' src='http://clickb8m8.github.io/articles/share-on-facebook.png'></img></a>";
 }
 
 var elms = document.getElementsByClassName("shrimg");
